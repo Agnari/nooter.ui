884a733902d3b30823326bb90cfa567507bb4bab
Ricardas <ricgembickis@gmail.com>
2022-10-04 16:58:27 +0300

    New

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello World, TEST
+          Hello World, TESTt
         </p>
         <a
           className="App-link"
