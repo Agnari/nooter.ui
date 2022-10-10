1bbc74924fd05ba4fed2f63b4efeaabc1e920aea
Nympherr <lukasstauskas@yahoo.com>
2022-10-10 08:55:39 +0300

    Įdėtas MUI.com
    Įkeltas material UI modulis.
    Instaliavau patį pagrindinį, kitų neinstaliavau(nežinojau ar reikalinga)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Demo from './MUI_demo';
 
 function App() {
   return (
     <div className="App">
+      <Demo />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/MUI_demo.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function Demo() {
+  return <Button variant="contained">MUI ĮDĖTAS</Button>;
+}
+export default Demo;
