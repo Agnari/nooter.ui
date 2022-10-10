44af5ab5d2e16517026e2d39e98605745a7d0928
Nympherr <lukasstauskas@yahoo.com>
2022-10-10 08:21:12 +0300

    Update App.tsx
    Testuoju ar viskas gaunasi

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Lukas has edited this</p>
         <a
           className="App-link"
           href="https://reactjs.org"
