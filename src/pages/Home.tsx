dff801b56f0829f54f72e6a820349a39ff69b114
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2022-11-11 19:17:00 +0200

    BCKGRND
    kad neskaudėtų akys pakeičiau iš čiūčia liūlia background į violetoviš

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,3 @@
 export function Home(){
-    return <h1 style={{fontFamily: "Righteous", fontSize: 100, textAlign: "center" }}>You are in home page!</h1>
+    return <h1 style={{fontFamily: "Righteous", fontSize: 110, textAlign: "center" }}>You are in the home page!</h1>
 }
