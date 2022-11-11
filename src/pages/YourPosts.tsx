57aa2ad187f95f8ef249d321ca4be7bc7f76e0f0
ernpuc <ernpuc@gmail.com>
2022-11-11 18:20:47 +0200

    Integrated with back-end

--- src/pages/YourPosts.tsx
+++ src/pages/YourPosts.tsx
@@ -7,24 +7,34 @@ import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
 import * as React from 'react';
 import Box from '@mui/material/Box';
-
-const title = ['Title 1', 'Title 2', 'Title 3', 'Title 4', 'Title 5']
-
+import { useEffect, useState } from "react";
 
 export function YourPosts() {
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        fetch("https://localhost:7018/api/articles")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setItems(result);
+                    console.log(result);
+                }
+            )
+    }, [])
+    
     return (
         <Container maxWidth="md">
-            <h2 style={{fontFamily: "Righteous"}}>Your Posts</h2>
+            <h2 style={{ fontFamily: "Righteous" }}>Your Posts</h2>
             <Grid container spacing={4}>
-                {title.map((title) => (
-                    <Grid item key={title} xs={12} sm={12} md={12}>
+                {items.map((articles:any) => (
+                    <Grid item key={articles.title} xs={12} sm={12} md={12}>
                         <Card
                             sx={{ height: '87%', display: 'flex', flexDirection: 'column', borderRadius: 3, background: "linear-gradient(80deg, darkgray, gray)", borderBottom: (theme) => `8px solid ${theme.palette.divider}`, borderStyle: "solid", borderColor: "#A97637" }}
                         >
                             <Stack direction="row">
                                 <CardContent sx={{ flexGrow: 1 }}>
-                                    <p style={{fontFamily: "Righteous", fontWeight: "bold", fontSize: 15, color: "#181818" }}>
-                                        {title}
+                                    <p style={{ fontFamily: "Righteous", fontWeight: "bold", fontSize: 15, color: "#181818" }}>
+                                        {articles.title}
                                     </p>
                                 </CardContent>
                                 <CardActions>
