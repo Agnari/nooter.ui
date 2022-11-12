15fcf5bae436ae7fcb6714c8cd6b557de35e63c2
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2022-11-13 00:21:43 +0200

    touchups

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import './animation.css';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
@@ -16,24 +17,23 @@ function App() {
   return (
     <>
       <AppBar
-        position="static"
+        position="absolute"
         color="default"
         elevation={0}
         sx={{height: "62px", background: "#242323"}}
       >
 
-        <Toolbar sx={{ flexWrap: 'wrap' }}>
-        <img src={require('./nootnoot.png')} alt="oops" style={{transform:"translate(70px) rotate(20deg)", height: '50px', width: '50px' }}/>
-          <Link variant="h6" color="inherit" href="/" underline="none" noWrap sx={{ flexGrow: 1, fontFamily: "Cabin Sketch", color: "white", fontSize: 27, transform: "translate(70px)" }}> 
-            NOOTER 
+        <Toolbar sx={{ flexWrap: 'wrap', transform: "translate(0px, -10px)" }}>
+          <Link variant="h6" color="inherit" href="/" underline="none" noWrap sx={{ flexGrow: 1, fontFamily: "Cabin Sketch", color: "white", fontSize: 27, transform: "translate(70px, -9px)", filter: "drop-shadow(3px 3px 3px #3d3d3d)" }}> 
+          <img src={require('./nootnoot.png')} alt="oops" style={{transform:"translate(0px, 15px) rotate(20deg)", height: '50px', width: '50px' }}/>NOOTER 
           </Link>
 
-          <Link href="/newpost" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-500px)"}}>  Post
+          <Link className="animation" href="/newpost" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-500px, 10px)"}}>  Post
           </Link>
-          <Link href="/yourposts" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-400px)"}}> 
+          <Link className="animation" href="/yourposts" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-400px, 10px)"}}> 
           Edit
           </Link>
-          <Button href="/login" variant="contained" sx={{ my: 1, mx: 1.5, backgroundColor: "#A97637", color: "white", borderRadius: 3, fontFamily: "Righteous", left: 20 }}>
+          <Button href="/login" variant="contained" sx={{ my: 1, mx: 1.5, backgroundColor: "#A97637", color: "white", borderRadius: 3, fontFamily: "Righteous", left: 20, filter: "drop-shadow(0px 1.5px 1.5px #404040)" }}>
             Login
           </Button>
         </Toolbar>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -33,10 +33,8 @@ export function Register() {
     }
 
     return (
-        <Container style={{background: "linear-gradient(to left, #A97637, #60759c)", borderRadius: 40}} sx={{width: '50%'}}>
-        <Container style={{background: "linear-gradient(45deg, white, #a8a7a7", borderRadius: 20}}>
+        <Container style={{background: "url(https://img.freepik.com/free-photo/vintage-crumpled-paper-textured_53876-96063.jpg?t=st=1668288152~exp=1668288752~hmac=4b7bab87ab2d6a0477777fd2584b043def4808c24a8e1eb81573ca3b5b83b122)"}} sx={{width:"60%", transform:"translate(0px, 100px)", border: 7, borderColor: "#2e2d2d", borderStyle:"dashed", borderTopLeftRadius: "255px 15px", borderTopRightRadius: "15px 225px", borderBottomRightRadius: "225px 15px", borderBottomLeftRadius: "15px 255px", filter:"drop-shadow(6px 6px 6px black)"}}>
         <Container component="main" maxWidth="xs">
-            <CssBaseline />
             <Box
                 sx={{
                     marginTop: 8,
@@ -67,7 +65,7 @@ export function Register() {
                         name="email"
                         autoComplete="email"
                         inputProps={{style:{fontFamily: "Righteous"}}}
-                        InputLabelProps={{style:{fontFamily: "Righteous"}}}
+                        InputLabelProps={{style:{fontFamily: "Cabin Sketch"}}}
                     />
                     <TextField
                         margin="normal"
@@ -79,28 +77,27 @@ export function Register() {
                         id="password"
                         autoComplete="new-password"
                         inputProps={{style:{fontFamily: "Righteous"}}}
-                        InputLabelProps={{style:{fontFamily: "Righteous"}}}
+                        InputLabelProps={{style:{fontFamily: "Cabin Sketch"}}}
                     />
                     <Button
                         type="submit"
                         fullWidth
                         variant="contained"
 
-                        sx={{ mt: 3, mb: 2, backgroundColor: "#A97637", borderRadius: 5, fontFamily: "Righteous" }}
+                        sx={{ mt: 3, mb: 2, backgroundColor: "#59260B", border: 2, borderStyle:'groove', borderColor:'#2b1204', fontFamily: "Cabin Sketch", color: "white", fontSize: 20 }}
                      
                     >
                         Sign Up
                     </Button>
                     <Grid container justifyContent="flex-end">
                         <Grid item style={{paddingBottom: 20}}>
-                            <Link href="/login" variant="body2" style={{fontFamily: "Righteous", color: "black"}}>
+                            <Link href="/login" variant="body2" underline="always" style={{fontFamily: "Righteous", color: "darkred"}}>
                                 Already have an account? Sign in
                             </Link>
                         </Grid>
                     </Grid>
                 </Box>
             </Box>
-        </Container>
         </Container>
         </Container>
     );
