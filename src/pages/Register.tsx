e1bb8350740d9aaeaa9aa0eb6682b612e69b8d55
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2022-11-13 21:42:39 +0200

    Revert "Prototipo cpy"

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -33,8 +33,10 @@ export function Register() {
     }
 
     return (
-        <Container style={{background: "url(https://img.freepik.com/free-photo/vintage-crumpled-paper-textured_53876-96063.jpg?t=st=1668288152~exp=1668288752~hmac=4b7bab87ab2d6a0477777fd2584b043def4808c24a8e1eb81573ca3b5b83b122)"}} sx={{width:"60%", transform:"translate(0px, 100px)", border: 7, borderColor: "#2e2d2d", borderStyle:"dashed", borderTopLeftRadius: "255px 15px", borderTopRightRadius: "15px 225px", borderBottomRightRadius: "225px 15px", borderBottomLeftRadius: "15px 255px", filter:"drop-shadow(6px 6px 6px black)"}}>
+        <Container style={{background: "linear-gradient(to left, #A97637, #60759c)", borderRadius: 40}} sx={{width: '50%'}}>
+        <Container style={{background: "linear-gradient(45deg, white, #a8a7a7", borderRadius: 20}}>
         <Container component="main" maxWidth="xs">
+            <CssBaseline />
             <Box
                 sx={{
                     marginTop: 8,
@@ -44,7 +46,7 @@ export function Register() {
                 }}
             >
                 <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                    <TextField 
+                    <TextField
                         margin="normal"
                         required
                         fullWidth
@@ -53,10 +55,10 @@ export function Register() {
                         name="username"
                         autoComplete="username"
                         inputProps={{style:{fontFamily: "Righteous"}}}
-                        InputLabelProps={{style:{fontFamily: "Cabin Sketch"}}}
+                        InputLabelProps={{style:{fontFamily: "Righteous"}}}
                         
                     />
-                    <TextField 
+                    <TextField
                         margin="normal"
                         required
                         fullWidth
@@ -65,9 +67,9 @@ export function Register() {
                         name="email"
                         autoComplete="email"
                         inputProps={{style:{fontFamily: "Righteous"}}}
-                        InputLabelProps={{style:{fontFamily: "Cabin Sketch"}}}
+                        InputLabelProps={{style:{fontFamily: "Righteous"}}}
                     />
-                    <TextField sx={{borderColor: "black", border: 1, borderRadius: 1, borderStyle:'groove'}}
+                    <TextField
                         margin="normal"
                         required
                         fullWidth
@@ -77,27 +79,28 @@ export function Register() {
                         id="password"
                         autoComplete="new-password"
                         inputProps={{style:{fontFamily: "Righteous"}}}
-                        InputLabelProps={{style:{fontFamily: "Cabin Sketch"}}}
+                        InputLabelProps={{style:{fontFamily: "Righteous"}}}
                     />
                     <Button
                         type="submit"
                         fullWidth
                         variant="contained"
 
-                        sx={{ mt: 3, mb: 2, backgroundColor: "#59260B", border: 2, borderStyle:'groove', borderColor:'#2b1204', fontFamily: "Cabin Sketch", color: "white", fontSize: 20 }}
+                        sx={{ mt: 3, mb: 2, backgroundColor: "#A97637", borderRadius: 5, fontFamily: "Righteous" }}
                      
                     >
                         Sign Up
                     </Button>
                     <Grid container justifyContent="flex-end">
                         <Grid item style={{paddingBottom: 20}}>
-                            <Link href="/login" variant="body2" underline="always" style={{fontFamily: "Righteous", color: "darkred"}}>
+                            <Link href="/login" variant="body2" style={{fontFamily: "Righteous", color: "black"}}>
                                 Already have an account? Sign in
                             </Link>
                         </Grid>
                     </Grid>
                 </Box>
             </Box>
+        </Container>
         </Container>
         </Container>
     );
