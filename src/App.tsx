b21b2ad6a8d848178c3cf851afaf0d644224d67c
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2022-11-13 14:44:17 +0200

    Small changes to your posts

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Link from '@mui/material/Link';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
 
 function App() {
   return (
@@ -27,10 +29,11 @@ function App() {
           <Link variant="h6" color="inherit" href="/" underline="none" noWrap sx={{ flexGrow: 1, fontFamily: "Cabin Sketch", color: "white", fontSize: 27, transform: "translate(70px, -9px)", filter: "drop-shadow(3px 3px 3px #3d3d3d)" }}> 
           <img src={require('./nootnoot.png')} alt="oops" style={{transform:"translate(0px, 15px) rotate(20deg)", height: '50px', width: '50px' }}/>NOOTER 
           </Link>
-
-          <Link className="animationshut" href="/newpost" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-500px, 10px)"}}>  Post
+          <PostAddIcon sx={{color:"white", transform:"translate(-417px, -2px)"}}/>
+          <DynamicFeedIcon sx={{color:"white", transform:"translate(-230px, 0px)"}}/>
+          <Link className="animationshut" href="/newpost" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-450px, 10px)"}}>  Post
           </Link>
-          <Link className="animationshut" href="/yourposts" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-400px, 10px)"}}> 
+          <Link className="animationshut" href="/yourposts" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-320px, 10px)"}}> 
           Edit
           </Link>
           <Button href="/login" variant="contained" sx={{ my: 1, mx: 1.5, backgroundColor: "#A97637", color: "white", borderRadius: 3, fontFamily: "Righteous", left: 20, filter: "drop-shadow(0px 1.5px 1.5px #404040)" }}>
--- src/pages/YourPosts.tsx
+++ src/pages/YourPosts.tsx
@@ -9,20 +9,23 @@ import Container from '@mui/material/Container';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
+import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
 const title = ['Title 1', 'Title 2', 'Title 3', 'Title 4', 'Title 5']
 
 
 export function YourPosts() {
     return (
-        <Container maxWidth="md" sx={{border: 5, borderColor: "black", transform:"translate(0px, 30px)", borderTopLeftRadius: "255px 15px", borderTopRightRadius: "15px 225px", borderBottomRightRadius: "225px 15px", borderBottomLeftRadius: "15px 255px", borderStyle: "dashed"}}>
-            <h2 className="hover1" style={{fontFamily: "Cabin Sketch", fontSize: 50, height:44, width:300}}>Your Posts</h2>
+        <Container maxWidth="md" sx={{border: 5, borderColor: "black", transform:"translate(0px, 30px)", borderTopLeftRadius: "255px 15px", borderTopRightRadius: "15px 225px", borderBottomRightRadius: "225px 15px", borderBottomLeftRadius: "15px 255px", borderStyle: "dashed", paddingBottom:15, filter:"drop-shadow(7px 7px 7px lightgrey)"}}>
+            
+            <h2 className="hover1" style={{fontFamily: "Cabin Sketch", fontSize: 50, height:44, width:300}}> <DoneOutlineIcon sx={{color:"black", transform:"rotate(3deg)"}}/>Your Posts </h2>
             <Grid container spacing={4} sx={{transform:"translate(20px)"}}>
                 {title.map((title) => (
                     <Grid item key={title} xs={12} sm={12} md={12}>
-                        <Card
-                            sx={{ height: '87%', display: 'flex', background:"transparent", flexDirection: 'column', borderRadius: 3, borderBottom: (theme) => `8px solid ${theme.palette.divider}`, borderColor: "#A97637", borderStyle:"dashed", borderBottomStyle:"double", filter:"drop-shadow(8px 8px 8px black)" }}
-                        >
+                        <CheckCircleOutlineIcon sx={{transform:"rotate(2deg) translate(-20px)", color:"darkred", filter:"drop-shadow(1px 1px 1px black)"}}/>
+                        <Card sx={{ height: '87%', width: '95%', display: 'flex', background:"transparent", flexDirection: 'column', borderRadius: 3, borderBottom: (theme) => `8px solid ${theme.palette.divider}`, borderColor: "#A97637", borderStyle:"dashed", borderBottomStyle:"double", filter:"drop-shadow(8px 8px 8px black)" }}>
                             <Stack direction="row">
                                 <CardContent sx={{ flexGrow: 1 }}>
                                     <p className="hover1" style={{fontFamily: "Cabin Sketch", fontSize: 25, color: "#181818", width: 70 }}>
@@ -30,7 +33,7 @@ export function YourPosts() {
                                     </p>
                                 </CardContent>
                                 <CardActions>
-                                    <Link className="hover-2" href="/editpost" underline="none" style={{ fontFamily: "Cabin Sketch", fontSize:20, color: "black", backgroundColor: "transparent", padding:"20px 20px 20px", transform:"translate(-6px)"}}> Edit</Link>
+                                    <Link className="hover-2" href="/editpost" underline="none" style={{ fontFamily: "Cabin Sketch", fontSize:20, color: "black", backgroundColor: "transparent", padding:"20px 20px 20px"}}> Edit</Link>
                                 </CardActions>
                             </Stack>
                         </Card>
