45a3608a932295a81458a029e3f363b78219439e
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2022-11-13 21:22:24 +0200

    final commit before merge

--- src/pages/EditPost.tsx
+++ src/pages/EditPost.tsx
@@ -6,13 +6,12 @@ import Grid from "@mui/material/Grid";
 
 export function EditPost() {
     return (
-        <Container style={{background: "linear-gradient(to left, #A97637, #60759c)", borderRadius: 40}} sx={{width: '83%'}}>
-        <Container style={{background: "linear-gradient(45deg, white, #a8a7a7", borderRadius: 20}}>
-        
-        <Container maxWidth="md">
-            <h2 style={{fontFamily: "Righteous", paddingTop: 15, fontSize: 30 }}>Edit Post</h2>
+        <Container style={{background: "url(https://www.tilingtextures.com/wp-content/uploads/2018/11/0068-1-512x512.jpg)", backgroundRepeat:"no-repeat", backgroundSize:"cover"}} sx={{width:"87%", border:5, transform:"translate(0px, 90px)", borderTopLeftRadius: "255px 15px", borderTopRightRadius: "15px 225px", borderBottomRightRadius: "225px 15px", borderBottomLeftRadius: "15px 255px", borderStyle: "dashed", filter:"drop-shadow(5px 5px 5px black)"}}>
+         <img src={require("../fuqno.png")} style={{height:'100px', width:'100px', filter:"drop-shadow(3px 3px 3px black)", transform:"translate(-60px, -35px) rotate(45deg)"}}/> <img src={require("../anber.png")} style={{height:100, width:100, filter:"drop-shadow(-3px 3px -3px black)", transform:"translate(900px, -45px) rotate(-10deg)"}}/>
+        <Container maxWidth="md" sx={{ transform:"translate(0px, -100px)", marginBottom:-10}}>
+            <h2 style={{fontFamily: "Calligraffitti, cursive", fontSize: 30 }}>Edit Post</h2>
 
-            <TextField
+            <TextField sx={{background:"url(https://img.freepik.com/free-photo/white-crumpled-paper-texture-background-design-space-white-tone_1258-78696.jpg?w=1800&t=st=1668364849~exp=1668365449~hmac=4f3352146117b55bd624ed922e88d5913590fe62d3eeaa38fbae504457d6644b)", transform:"rotate(-1deg)"}}
                 margin="normal"
                 variant="standard"
                 required
@@ -22,7 +21,7 @@ export function EditPost() {
                 InputLabelProps={{style:{fontFamily: "Righteous"}}}
 
             />
-            <TextField
+            <TextField sx={{background:"url(https://img.freepik.com/free-photo/white-crumpled-paper-texture-background-design-space-white-tone_1258-78696.jpg?w=1800&t=st=1668364849~exp=1668365449~hmac=4f3352146117b55bd624ed922e88d5913590fe62d3eeaa38fbae504457d6644b)", transform:"rotate(-2deg)"}}
                 placeholder="Edit Post..."
                 multiline
                 fullWidth
@@ -37,13 +36,12 @@ export function EditPost() {
                         type="submit"
                         fullWidth
                         variant="contained"
-                        sx={{ mt: 3, mb: 2, backgroundColor: "#A97637", borderRadius: 10, fontFamily: "Righteous" }}
+                        sx={{ mt: 3, mb: 2, fontFamily: "Calligraffitti", backgroundColor: "#59260B", borderRadius: "50%" }}
                     >
                         Submit Changes
                     </Button>
                 </Grid>
             </Grid>
-        </Container>
         </Container>
         </Container>
     )
--- src/pages/YourPosts.tsx
+++ src/pages/YourPosts.tsx
@@ -18,14 +18,14 @@ const title = ['Title 1', 'Title 2', 'Title 3', 'Title 4', 'Title 5']
 
 export function YourPosts() {
     return (
-        <Container maxWidth="md" sx={{border: 5, borderColor: "black", transform:"translate(0px, 30px)", borderTopLeftRadius: "255px 15px", borderTopRightRadius: "15px 225px", borderBottomRightRadius: "225px 15px", borderBottomLeftRadius: "15px 255px", borderStyle: "dashed", paddingBottom:15, filter:"drop-shadow(7px 7px 7px lightgrey)"}}>
+        <Container maxWidth="md" sx={{background:"url(https://www.tilingtextures.com/wp-content/uploads/2018/11/0068-1-512x512.jpg)", border: 5, borderColor: "black", transform:"translate(0px, 30px)", borderTopLeftRadius: "255px 15px", borderTopRightRadius: "15px 225px", borderBottomRightRadius: "225px 15px", borderBottomLeftRadius: "15px 255px", borderStyle: "dashed", paddingBottom:15, filter:"drop-shadow(7px 7px 7px black)"}}>
             
             <h2 className="hover1" style={{fontFamily: "Cabin Sketch", fontSize: 50, height:44, width:300}}> <DoneOutlineIcon sx={{color:"black", transform:"rotate(3deg)"}}/>Your Posts <img src={require("../wtf.png")} alt="lol no" style={{height:"30px", width:"30px", transform:"rotate(3deg)"}}/></h2>
             <Grid container spacing={4} sx={{transform:"translate(20px)"}}>
                 {title.map((title) => (
                     <Grid item key={title} xs={12} sm={12} md={12}>
-                        <CheckCircleOutlineIcon sx={{transform:"translate(-20px)", color:"darkred", filter:"drop-shadow(1px 1px 1px black)"}}/>
-                        <Card sx={{ height: '87%', width: '95%', display: 'flex', background:"transparent", flexDirection: 'column', borderRadius: 3, borderBottom: (theme) => `8px solid ${theme.palette.divider}`, borderColor: "#A97637", borderStyle:"dashed", borderBottomStyle:"double", filter:"drop-shadow(8px 8px 8px black)" }}>
+                        <CheckCircleOutlineIcon sx={{transform:"translate(-28px, 20px)", color:"darkred", filter:"drop-shadow(1px 1px 1px black)"}}/>
+                        <Card sx={{ height: '87%', width: '95%', display: 'flex', background:"transparent", flexDirection: 'column', borderBottom: (theme) => `5px solid ${theme.palette.divider}`, borderColor: "#41424C", borderStyle:"solid", borderTopLeftRadius: "255px 15px", borderTopRightRadius: "15px 225px", borderBottomRightRadius: "225px 15px", borderBottomLeftRadius: "15px 255px" }}>
                             <Stack direction="row">
                                 <CardContent sx={{ flexGrow: 1 }}>
                                     <p className="hover1" style={{fontFamily: "Cabin Sketch", fontSize: 35, color: "#181818", width: 100 }}>
