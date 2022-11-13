ff2ca235ab08ef83ec4536f739a98e17957aad7c
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2022-11-14 00:40:10 +0200

    Pakeistas App.tsx (appbar, routes)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import './animation.css';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
@@ -11,28 +12,33 @@ import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Link from '@mui/material/Link';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
 
 function App() {
   return (
     <>
       <AppBar
-        position="static"
+        position="absolute"
         color="default"
         elevation={0}
-        sx={{ borderBottom: (theme) => `10px solid ${theme.palette.divider}`, backgroundColor: "#242323", borderColor: "#ce5704", borderBottomLeftRadius: 20, borderBottomRightRadius: 20, borderBottomStyle: "groove" }}
+        sx={{height: "62px", background: "#242323"}}
       >
 
-        <Toolbar sx={{ flexWrap: 'wrap' }}>
-          <Link variant="h6" color="inherit" href="/" underline="hover" noWrap sx={{ flexGrow: 1, fontFamily: "Righteous", color: "white", fontSize: 27 }}>
-            NOOTER
+        <Toolbar sx={{ flexWrap: 'wrap', transform: "translate(0px, -10px)" }}>
+
+          <Link variant="h6" color="inherit" href="/" underline="none" noWrap sx={{ flexGrow: 1, fontFamily: "Cabin Sketch", color: "white", fontSize: 27, transform: "translate(70px, -9px)", filter: "drop-shadow(3px 3px 3px #3d3d3d)" }}> 
+          <img src={require('./nootnoot.png')} alt="oops" style={{transform:"translate(0px, 15px) rotate(20deg)", height: '50px', width: '50px' }}/>NOOTER 
           </Link>
 
-          <Button href="/newpost" variant="outlined" sx={{ my: 1, mx: 1.5, borderColor: "#A97637", color: "#A97637", borderRadius: 20, left: 30 }}>  New Post
-          </Button>
-          <Button href="/yourposts" variant="outlined" sx={{ my: 1, mx: 1.5, color: "#A97637", borderColor: "#A97637", borderRadius: 20, left: 30 }}> 
-          Your Posts
-          </Button>
-          <Button href="/login" variant="contained" sx={{ my: 1, mx: 1.5, backgroundColor: "#A97637", color: "white", borderRadius: 3, fontFamily: "Righteous", left: 20 }}>
+          <PostAddIcon sx={{color:"white", transform:"translate(-417px, -2px)"}}/>
+          <DynamicFeedIcon sx={{color:"white", transform:"translate(-230px, 0px)"}}/>
+          <Link className="animationshut" href="/newpost" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-450px, 10px)"}}>  Post
+          </Link>
+          <Link className="animationshut" href="/yourposts" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-320px, 10px)"}}> 
+          Edit
+          </Link>
+          <Button href="/login" variant="contained" sx={{ my: 1, mx: 1.5, backgroundColor: "#A97637", color: "white", borderRadius: 3, fontFamily: "Righteous", left: 20, filter: "drop-shadow(0px 1.5px 1.5px #404040)" }}>
             Login
           </Button>
         </Toolbar>
