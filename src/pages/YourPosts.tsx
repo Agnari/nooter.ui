f1c18ea08027df11f4089ddfb2c2af5ce26621c9
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2022-11-13 15:54:25 +0200

    Added stuff

--- src/pages/YourPosts.tsx
+++ src/pages/YourPosts.tsx
@@ -20,15 +20,15 @@ export function YourPosts() {
     return (
         <Container maxWidth="md" sx={{border: 5, borderColor: "black", transform:"translate(0px, 30px)", borderTopLeftRadius: "255px 15px", borderTopRightRadius: "15px 225px", borderBottomRightRadius: "225px 15px", borderBottomLeftRadius: "15px 255px", borderStyle: "dashed", paddingBottom:15, filter:"drop-shadow(7px 7px 7px lightgrey)"}}>
             
-            <h2 className="hover1" style={{fontFamily: "Cabin Sketch", fontSize: 50, height:44, width:300}}> <DoneOutlineIcon sx={{color:"black", transform:"rotate(3deg)"}}/>Your Posts </h2>
+            <h2 className="hover1" style={{fontFamily: "Cabin Sketch", fontSize: 50, height:44, width:300}}> <DoneOutlineIcon sx={{color:"black", transform:"rotate(3deg)"}}/>Your Posts <img src={require("../wtf.png")} alt="lol no" style={{height:"30px", width:"30px", transform:"rotate(3deg)"}}/></h2>
             <Grid container spacing={4} sx={{transform:"translate(20px)"}}>
                 {title.map((title) => (
                     <Grid item key={title} xs={12} sm={12} md={12}>
-                        <CheckCircleOutlineIcon sx={{transform:"rotate(2deg) translate(-20px)", color:"darkred", filter:"drop-shadow(1px 1px 1px black)"}}/>
+                        <CheckCircleOutlineIcon sx={{transform:"translate(-20px)", color:"darkred", filter:"drop-shadow(1px 1px 1px black)"}}/>
                         <Card sx={{ height: '87%', width: '95%', display: 'flex', background:"transparent", flexDirection: 'column', borderRadius: 3, borderBottom: (theme) => `8px solid ${theme.palette.divider}`, borderColor: "#A97637", borderStyle:"dashed", borderBottomStyle:"double", filter:"drop-shadow(8px 8px 8px black)" }}>
                             <Stack direction="row">
                                 <CardContent sx={{ flexGrow: 1 }}>
-                                    <p className="hover1" style={{fontFamily: "Cabin Sketch", fontSize: 25, color: "#181818", width: 70 }}>
+                                    <p className="hover1" style={{fontFamily: "Cabin Sketch", fontSize: 35, color: "#181818", width: 100 }}>
                                         {title}
                                     </p>
                                 </CardContent>
