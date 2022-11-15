e2aa4fad34165dd62bca218ae2d6615cde54c49a
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2022-11-15 16:09:15 +0200

    Appbar scalingas
    appbar pritaikytas visoms rezoliucijoms

--- src/App.tsx
+++ src/App.tsx
@@ -28,20 +28,20 @@ function App() {
         sx={{height: "62px", background: "#242323"}}
       >
 
-        <Toolbar sx={{ flexWrap: 'wrap', transform: "translate(0px, -10px)", borderBottomColor:"#f7b500", borderBottomStyle:"dashed" }}>
+        <Toolbar sx={{ flexWrap: 'wrap', transform:"translate(0vh, -1vh)", borderBottomColor:"#f7b500", borderBottomStyle:"dashed" }}>
 
-          <Link variant="h6" color="inherit" href="/" underline="none" noWrap sx={{ flexGrow: 1, fontFamily: "Cabin Sketch", color: "white", fontSize: 27, transform: "translate(70px, -9px)", filter: "drop-shadow(3px 3px 3px #3d3d3d)" }}> 
-          <img src={require('./nootnoot.png')} alt="oops" style={{transform:"translate(0px, 15px) rotate(20deg)", height: '50px', width: '50px' }}/>NOOTER 
+          <Link variant="h6" color="inherit" href="/" underline="none" noWrap sx={{ flexGrow: 1, fontFamily: "Cabin Sketch", color: "white", fontSize: 27, transform: "translate(1vw, -2vh)", filter: "drop-shadow(3px 3px 3px #3d3d3d)" }}> 
+          <img src={require('./nootnoot.png')} alt="oops" style={{transform:"translate(0vw, 2.5vh) rotate(20deg)", height: '50px', width: '50px' }}/>NOOTER 
           </Link>
 
-          <PostAddIcon sx={{color:"white", transform:"translate(-417px, -2px)"}}/>
-          <DynamicFeedIcon sx={{color:"white", transform:"translate(-230px, 0px)"}}/>
-          <Link className="animationshut" href="/newpost" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-450px, 10px)"}}> Noot
+          <PostAddIcon sx={{color:"white", transform:"translate(-33vw, -0.5vh)"}}/>
+          <DynamicFeedIcon sx={{color:"white", transform:"translate(-18vw, -0.1vh)"}}/>
+          <Link className="animationshut" href="/newpost" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-35.7vw, 1.4vh)"}}> Noot
           </Link>
-          <Link className="animationshut" href="/yourposts" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-320px, 10px)"}}> 
+          <Link className="animationshut" href="/yourposts" underline="none" sx={{fontFamily: "Cabin Sketch", color: "white", fontSize: "24px", transform: "translate(-25.1vw, 1.5vh)"}}> 
           Stuff
           </Link>
-          <Button href="/login" variant="contained" sx={{ my: 1, mx: 1.5, backgroundColor: "#A97637", color: "white", borderRadius: 3, fontFamily: "Righteous", left: 20, filter: "drop-shadow(0px 1.5px 1.5px #404040)" }}>
+          <Button href="/login" variant="contained" sx={{ my: 1, mx: 1.5, backgroundColor: "#A97637", color: "white", borderRadius: 3, fontFamily: "Righteous", left: 20, filter: "drop-shadow(0px 1.5px 1.5px #404040)", transform:"translate(0vw, -0.5vh)"}}>
             Login
           </Button>
           <img className="hewalkin" src={require("./goose-running-goose.gif")}/>
