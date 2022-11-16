c64483cc8db46fcf1aedded790407c06d8eb6165
ernpuc <ernpuc@gmail.com>
2022-11-16 15:00:03 +0200

    Added a delete option in YourPosts page

--- src/pages/YourPosts.tsx
+++ src/pages/YourPosts.tsx
@@ -5,8 +5,6 @@ import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
-import * as React from 'react';
-import Box from '@mui/material/Box';
 import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 
@@ -22,12 +20,17 @@ export function YourPosts() {
                 }
             )
     }, [])
-    
+
+    const deletePost = (id: string) => {
+        fetch("https://localhost:7018/api/articles/" + id, { method: 'DELETE' })
+        setItems((items) => items.filter((article: any) => article.id !== id))
+    }
+
     return (
         <Container maxWidth="md">
             <h2 style={{ fontFamily: "Righteous" }}>Your Posts</h2>
             <Grid container spacing={4}>
-                {items.map((articles:any) => (
+                {items.map((articles: any) => (
                     <Grid item key={articles.title} xs={12} sm={12} md={12}>
                         <Card
                             sx={{ height: '87%', display: 'flex', flexDirection: 'column', borderRadius: 3, borderBottom: (theme) => `8px solid ${theme.palette.divider}`, borderStyle: "solid", borderColor: "#A97637" }}
@@ -39,7 +42,8 @@ export function YourPosts() {
                                     </p>
                                 </CardContent>
                                 <CardActions>
-                                    <Button component={Link} to={'/editpost/'+articles.id} size="small" variant="contained" style={{ fontFamily: "Righteous", color: "black", backgroundColor: "#A97637", borderRadius: 20 }} sx={{ border: 0.7, borderColor: "darkgrey" }}>Edit</Button>
+                                    <Button component={Link} to={'/editpost/' + articles.id} size="small" variant="contained" style={{ fontFamily: "Righteous", color: "black", backgroundColor: "#A97637", borderRadius: 20 }} sx={{ border: 0.7, borderColor: "darkgrey" }}>Edit</Button>
+                                    <Button onClick={() => deletePost(articles.id)} size="small" variant="contained" style={{ fontFamily: "Righteous", color: "black", backgroundColor: "#A97637", borderRadius: 20 }} sx={{ border: 0.7, borderColor: "darkgrey" }}>Delete</Button>
                                 </CardActions>
                             </Stack>
                         </Card>
