8e6cb95e756ffd3a6702c857f7f7d96dc6852e2e
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2022-11-16 18:57:12 +0200

    Your posts fix
    Fixed buttons dissapearing behind container border

--- src/pages/YourPosts.tsx
+++ src/pages/YourPosts.tsx
@@ -31,8 +31,8 @@ export function YourPosts() {
     }
     
     return (
-        <Container maxWidth="md" sx={{ background: "url(https://www.tilingtextures.com/wp-content/uploads/2018/11/0068-1-512x512.jpg)", border: 5, borderColor: "black", transform: "translate(0px, 30px)", borderTopLeftRadius: "255px 15px", borderTopRightRadius: "15px 225px", borderBottomRightRadius: "225px 15px", borderBottomLeftRadius: "15px 255px", borderStyle: "dashed", paddingBottom: 15, filter: "drop-shadow(7px 7px 7px black)" }}>
-            <h2 className="hover1" style={{ fontFamily: "Cabin Sketch", fontSize: 50, height: 44, width: 300 }}> <DoneOutlineIcon sx={{ color: "black", transform: "rotate(3deg)" }} />Your Posts
+        <Container className="paperStack" maxWidth="md" >
+            <h2 className="hover1" style={{ fontFamily: "Cabin Sketch", fontSize: 50, height: 44, width: 300 }}> <DoneOutlineIcon sx={{ color: "black" }} />Your Posts
                 <img className="ducktrigger" src={require("../stickers/wtf.png")} alt="lol no" style={{ height: "30px", width: "30px", transform: "rotate(3deg)" }} />
                 Your Posts
             </h2>
@@ -41,17 +41,17 @@ export function YourPosts() {
                     <Grid item key={articles.title} xs={12} sm={12} md={12}>
                         <CheckCircleOutlineIcon sx={{ transform: "translate(-28px, 20px)", color: "darkred", filter: "drop-shadow(1px 1px 1px black)" }} />
 
-                        <Card sx={{ height: '87%', width: '95%', display: 'flex', background: "transparent", flexDirection: 'column', borderBottom: (theme) => `5px solid ${theme.palette.divider}`, borderColor: "#41424C", borderStyle: "solid", borderTopLeftRadius: "255px 15px", borderTopRightRadius: "15px 225px", borderBottomRightRadius: "225px 15px", borderBottomLeftRadius: "15px 255px" }}>
+                        <Card sx={{ height: '87%', width: '95%', display: 'flex', background: "../backgrounds/wall.jpg", flexDirection: 'column', borderBottom: (theme) => `5px solid ${theme.palette.divider}`, borderColor: "#41424C", borderStyle: "solid", borderTopLeftRadius: "255px 15px", borderTopRightRadius: "15px 225px", borderBottomRightRadius: "225px 15px", borderBottomLeftRadius: "15px 255px", filter:"invert(7%)" }}>
                             <Stack direction="row">
                                 <CardContent sx={{ flexGrow: 1 }}>
-                                    <p className="hover1" style={{ fontFamily: "Cabin Sketch", fontSize: 35, color: "#181818", maxWidth: "50vw" }}>
+                                    <p className="hover1" style={{ fontFamily: "Cabin Sketch", fontSize: 35, color: "#181818", maxWidth: "45.5vw" }}>
                                         {articles.title}
                                     </p>
                                 </CardContent>
                                 <CardActions>
 
-                                    <Button className="hover-2" component={Link} to={'/editpost/' + articles.id} size="small" variant="contained" style={{ fontFamily: "Cabin Sketch", fontSize: 20, color: "black", backgroundColor: "transparent", padding: "10px 20px 20px" }}>Edit</Button>
-                                    <Button onClick={() => deletePost(articles.id)} size="small" variant="contained" style={{ fontFamily: "Righteous", color: "black", backgroundColor: "#A97637", borderRadius: 20 }} sx={{ border: 0.7, borderColor: "darkgrey" }}>Delete</Button>
+                                    <Button className="hover-2" component={Link} to={'/editpost/' + articles.id} size="small" variant="contained" style={{ fontFamily: "Cabin Sketch", fontSize: 20, color: "black", backgroundColor: "#C09372", padding: "10px 15px 20px" }}>Edit</Button>
+                                    <Button className="hover-2" onClick={() => deletePost(articles.id)} size="small" variant="contained" style={{ fontFamily: "Cabin Sketch", fontSize: 20, color: "black", backgroundColor:"#8B0000", padding: "10px 15px 20px" }}>Delete</Button>
 
                                 </CardActions>
                             </Stack>
