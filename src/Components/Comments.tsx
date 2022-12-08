b24d723aaf53b0120c82e6512746805c83824581
Ricka12 <ricgembickis@gmail.com>
2022-12-08 22:58:58 +0200

    Veikiantis comment
    Commentai susieti su back ir front tiesiog ju išdėstimas prastas REIKES PAKEISTI

--- src/Components/Comments.tsx
+++ src/Components/Comments.tsx
@@ -1,37 +1,67 @@
 import { Avatar, Box, Button, Card, CardContent, Container, Divider, Grid, List, ListItem, ListItemAvatar, ListItemText, Popover, TextField, Typography, Stack } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import SendIcon from '@mui/icons-material/Send';
 import React from "react";
-export function Comments() {
+import Swal from 'sweetalert2'
+import { getToken } from "../utils/auth";
 
+export function Comments() {
+    const userStr = localStorage.getItem("USER");
+    const [user, setUser] = useState(userStr ? JSON.parse(userStr) : null);
+    const Swal = require('sweetalert2');
+    const navigate = useNavigate();
     const { id } = useParams()
     const [text, setText] = useState("");
     const [UserName, setUserName] = useState("");
     const [items, setItems] = useState([]);
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            text: data.get('text'),
+            authorId: user.userId,
+        });
+
+        const token = getToken();
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                text: data.get('text'),
+                articleId: id,
+
+            })
+        };
+        if (data.get('text') === '') {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Text is missing',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+        }
+        fetch('https://localhost:7018/api/comments/', requestOptions)
+        .then (()=>navigate(window.location.pathname))
+
+    }
     useEffect(() => {
         fetch("https://localhost:7018/api/comments/" + id)
             .then(res => res.json())
             .then(
                 (result) => {
-                    setText(result.text);
-                    setUserName(result.UserName);
                     setItems(result);
                     console.log(result);
                 }
             )
     }, [id])
+    console.log(items);
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            title: data.get('title'),
-            Text: data.get('text'),
-            imageURL: data.get('photo'),
-        });
-    }
+
 
     return (
         <>
@@ -46,46 +76,38 @@ export function Comments() {
 
                             </CardContent>
                             <List >
-                                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1, width: "70vw", paddingTop: '5vh' }}>
+                                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1, paddingTop: '5vh' }}>
                                     <Card sx={{ border: 1, borderRadius: '50px' }}>
                                         <ListItem alignItems="flex-start">
                                             <ListItemAvatar>
                                                 <Avatar alt="?" src="/static/images/avatar/1.jpg" />
                                             </ListItemAvatar>
-                                            <ListItemText
-                                                primary="Username"
-                                                secondary={
-                                                    <React.Fragment>
-                                                        <Typography
-                                                            sx={{ display: 'inline' }}
-                                                            component="span"
-                                                            variant="body2"
-                                                            color="text.primary"
-                                                        >
-                                                        </Typography>
-                                                        <TextField
-                                                            margin="none"
-                                                            variant="standard"
-                                                            required
-                                                            fullWidth
-                                                            id="title"
-                                                            placeholder="Comment here"
-                                                            name='title'
-                                                        >
 
-                                                        </TextField>
-                                                    </React.Fragment>
-
-                                                }
-                                            />
+                                            <React.Fragment>
+                                                <Typography
+                                                    sx={{ display: 'inline' }}
+                                                    component="span"
+                                                    variant="body2"
+                                                    color="text.primary"
+                                                />
+                                                <TextField
+                                                    variant="standard"
+                                                    fullWidth
+                                                    id="text"
+                                                    placeholder="Comment here"
+                                                    name='text'
+                                                />
+                                                <Button
+                                                    type="submit"
+                                                    variant="contained">
+                                                    Submit
+                                                </Button>
+                                            </React.Fragment>
                                         </ListItem>
                                     </Card>
                                 </Box>
                                 <Grid container spacing={0.5} sx={{ transform: "translate(1.5873015873015872vw, -3vw)" }}>
-
-                                    {items && items.map((Comment: any) => (
-                                        <Stack spacing={4}>
-                                            <Stack direction='row' spacing={2}>
+                                            {items && items.map((Comment: any) => (
                                                 <Grid item key={Comment.id}>
                                                     <Card sx={{ border: 1, borderRadius: '50px' }}>
                                                         <ListItem alignItems="flex-start">
@@ -93,7 +115,7 @@ export function Comments() {
                                                                 <Avatar alt="?" src="/static/images/avatar/1.jpg" />
                                                             </ListItemAvatar>
                                                             <ListItemText
-                                                                primary="Username"
+                                                                primary={Comment.commenterName}
                                                                 secondary={
                                                                     <React.Fragment>
                                                                         <Typography
@@ -103,20 +125,19 @@ export function Comments() {
                                                                             color="text.primary"
                                                                         >
                                                                         </Typography>
-                                                                        {text}
+                                                                        {Comment.text}
                                                                     </React.Fragment>
                                                                 }
                                                             />
                                                         </ListItem>
                                                     </Card>
                                                 </Grid>
-                                            </Stack>
-                                        </Stack>
-                                    ))}
+                                            ))}
                                 </Grid>
                                 < Divider variant="inset" component="li" />
                             </List>
                         </Card>
+
                     </Container>
                 </Box>
             </main>
