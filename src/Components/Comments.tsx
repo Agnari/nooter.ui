7072eda9910c3647ccc07b0024387ad835806faa
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2022-12-20 21:41:43 +0200

    Comments

--- src/Components/Comments.tsx
+++ src/Components/Comments.tsx
@@ -75,20 +75,12 @@ export function Comments() {
     return (
         <>
             <main>
-                <Container sx={{ width: '30vw', marginTop:2 }}>
-                    <Card sx={{ paddingBottom: "5vw", borderRadius: 3, border: 5, borderStyle: "dashed", background: "linear-gradient(45deg, white, lightgrey)" }}>
-                        <CardContent sx={{ size: "fixed", marginLeft: 5 }}>
-                            <Typography variant="h4" align="center" sx={{ overflowWrap: "break-word", fontFamily: "Righteous" }}>
-                                Comments
-                            </Typography>
-                        </CardContent>
+                <Container sx={{ width: '30vw', marginTop: 2 }}>
+                    <Card sx={{ paddingBottom: "5vw", borderRadius: 3, border: 5, borderStyle: "dashed", background: "linear-gradient(45deg, white, lightgrey)", padding: 1 }}>
                         <List >
                             {user && <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                                <Card sx={{ border: 1, borderRadius: '50px' }}>
+                                <Card sx={{ border: 1, borderRadius: '50px', marginBottom: 3, borderBottom: 2 }}>
                                     <ListItem alignItems="flex-start">
-                                        <ListItemAvatar>
-                                            <Avatar alt="?" src="/static/images/avatar/1.jpg" />
-                                        </ListItemAvatar>
                                         <Container sx={{ display: "flex", fkexDirection: "row" }}>
                                             <React.Fragment>
                                                 <Typography
@@ -123,13 +115,10 @@ export function Comments() {
                                     <Grid item key={Comment.id}>
                                         <Card sx={{ border: 1, borderRadius: '50px', maxWidth: "25vw" }}>
                                             <ListItem alignItems="flex-start">
-                                                <ListItemAvatar>
-                                                    <Avatar alt="?" src="/static/images/avatar/1.jpg" />
-                                                </ListItemAvatar>
-                                                <ListItemText sx={{}}
-                                                    primary={Comment.commenterName}
+                                                <ListItemText disableTypography
+                                                    primary={Comment.commenterName} style={{ fontFamily: "Righteous" }}
                                                     secondary={
-                                                        <React.Fragment>
+                                                        <Container sx={{ wordBreak: "break-all" }}>
                                                             <Typography
                                                                 sx={{ display: 'inline' }}
                                                                 component="span"
@@ -138,7 +127,7 @@ export function Comments() {
                                                             >
                                                             </Typography>
                                                             {Comment.text}
-                                                        </React.Fragment>
+                                                        </Container>
                                                     }
                                                 />
                                             </ListItem>
